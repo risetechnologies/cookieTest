e34ae23ad7687a2a83ab8e3d270aab6124511ad1
menelike <mitschnacker@gmx.net>
2019-10-13 11:59:50 +0200

    setup runtime tests in order to test cookie behavior on browser and Cordova (which doesn't support cookies without workarounds)

---
+++ client/main.js
@@ -0,0 +1,189 @@
+import { Cookies } from "meteor/ostrio:cookies";
+import { HTTP } from "meteor/http";
+import { Meteor } from "meteor/meteor";
+
+const attachMsg = (node) => {
+  const list = document.getElementById("output");
+  list.appendChild(node);
+};
+
+const createListItem = (msg, style) => {
+  const node = document.createElement("li");
+  node.appendChild(document.createTextNode(msg));
+  node.setAttribute("style", style);
+  return node;
+};
+
+const assertEqual = (a, b, msg) => {
+  const node = createListItem(msg, `color: ${a === b ? "green" : "red"};`);
+  attachMsg(node);
+};
+
+const runTests = async () => {
+  const cookieValue = `${Date.now()}`;
+  const cookieParam = `__TEST_COOKIE__=${cookieValue}`;
+  const ___expectedCookies___ = encodeURI(cookieParam);
+  const cookieResetPath = Meteor.absoluteUrl("/__cookie_reset");
+  const cookieTestPath = Meteor.absoluteUrl("/__cookie_match");
+  const cookies = new Cookies({ allowQueryStringCookies: true });
+
+  cookies.remove();
+  assertEqual(document.cookie, "", "cookie is empty");
+  await new Promise(resolve => {
+    HTTP.get(
+      cookieResetPath,
+      {
+        params: {
+          ___removeCookie___: "__TEST_COOKIE__"
+        },
+        beforeSend(xhr) {
+          xhr.withCredentials = true;
+          return true;
+        }
+      },
+      (err, res) => {
+        assertEqual(res.statusCode, 200, "clear cookie request successful");
+        resolve();
+      }
+    );
+  });
+
+  await new Promise(resolve => {
+    HTTP.get(
+      cookieTestPath,
+      {
+        params: {
+          ___expectedCookies___: "undefined"
+        },
+        beforeSend(xhr) {
+          xhr.withCredentials = true;
+          return true;
+        }
+      },
+      (err, res) => {
+        assertEqual(res.statusCode, 200, "server did not receive cookie");
+        resolve();
+      }
+    );
+  });
+
+  cookies.set("__TEST_COOKIE__", cookieValue);
+  assertEqual(document.cookie, cookieParam, "set cookie on client");
+
+  if (Meteor.isCordova) {
+    await new Promise(resolve => {
+      HTTP.get(
+        cookieTestPath,
+        {
+          params: {
+            ___expectedCookies___: "undefined"
+          },
+          beforeSend(xhr) {
+            xhr.withCredentials = true;
+            return true;
+          }
+        },
+        (err, res) => {
+          assertEqual(res.statusCode, 200, "no cookie received from cordova");
+          resolve();
+        }
+      );
+    });
+  } else {
+    await new Promise(resolve => {
+      HTTP.get(
+        cookieTestPath,
+        {
+          params: {
+            ___expectedCookies___
+          }
+        },
+        (err, res) => {
+          assertEqual(
+            res.statusCode,
+            200,
+            "server received cookie from browser"
+          );
+          resolve();
+        }
+      );
+    });
+  }
+
+  await new Promise(resolve => cookies.send(resolve));
+
+  // in cordova cookies set by the server are invisible by the client
+  // but are sent within requests if withCredentials is true
+  await new Promise(resolve => {
+    HTTP.get(
+      cookieTestPath,
+      {
+        params: {
+          ___expectedCookies___
+        },
+        beforeSend(xhr) {
+          xhr.withCredentials = true;
+          return true;
+        }
+      },
+      (err, res) => {
+        assertEqual(res.statusCode, 200, "server received cookie after send()");
+        resolve();
+      }
+    );
+  });
+
+  if (Meteor.isCordova) {
+    await new Promise(resolve => {
+      HTTP.get(
+        cookieTestPath,
+        {
+          params: {
+            ___expectedCookies___: "undefined"
+          }
+        },
+        (err, res) => {
+          assertEqual(
+            res.statusCode,
+            200,
+            "no cookie from cordova with withCredentials = false"
+          );
+          resolve();
+        }
+      );
+    });
+  } else {
+    await new Promise(resolve => {
+      HTTP.get(
+        cookieTestPath,
+        {
+          params: {
+            ___expectedCookies___
+          }
+        },
+        (err, res) => {
+          assertEqual(
+            res.statusCode,
+            200,
+            "server received cookie from browser with withCredentials = false"
+          );
+          resolve();
+        }
+      );
+    });
+  }
+
+  attachMsg(createListItem("all tests run!"));
+};
+
+console.log(document.readyState);
+
+const func = () => {
+  if (document.readyState === "complete") {
+    runTests().catch(console.error);
+  } else {
+    setTimeout(func, 10)
+  }
+};
+
+func();
---
+++ server/main.js
@@ -0,0 +1,45 @@
+import { Cookies } from "meteor/ostrio:cookies";
+
+new Cookies({
+  auto: true,
+  allowQueryStringCookies: true
+});
+
+WebApp.connectHandlers.use("/__cookie_reset", (req, res) => {
+  const key = decodeURI(req.query.___removeCookie___);
+
+  if (req.headers.origin) {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Set-Cookie", [
+    `${key}=""; Path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`
+  ]);
+
+  res.writeHead(200);
+  res.end();
+});
+
+WebApp.connectHandlers.use("/__cookie_match", (req, res) => {
+  const query = decodeURI(req.query.___expectedCookies___);
+
+  if (req.headers.origin) {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // make sure we use the stringified req.headers.cookie}in order to match undefined as a string
+  if (`${req.headers.cookie}` === query) {
+    console.log(`expected: ${query}`);
+    console.log(`got: ${req.headers.cookie}`);
+    console.log("PASS");
+    res.writeHead(200);
+  } else {
+    console.error(`expected: ${query}`);
+    console.error(`got: ${req.headers.cookie}`);
+    console.log("FAIL");
+    res.writeHead(500);
+  }
+
+  res.end();
+});
